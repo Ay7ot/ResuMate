eddd9595ccd2f6edea42b086491f60bea92080a4
Aydot <ayomidotzee@gmail.com>
2023-06-22 07:34:47 -0700

    Added routing on navbar link

---
+++ client/src/Components/TemplateComponents/Porto.tsx
@@ -0,0 +1,12 @@
+import { useRef } from "react"
+
+export default function Porto() {
+
+    const itemref = useRef<HTMLDivElement | null>(null)
+
+    return (
+        <div className='flex justify-between rounded-md min-h-[1030px] bg-[#f4f5fd]' ref={itemref}>
+        
+        </div>
+    )
+}
