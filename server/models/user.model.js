3486e35068928a59ccad3841e72250cc6c071719
Aydot <ayomidotzee@gmail.com>
2023-08-27 07:05:04 -0700

    Database and server working

---
+++ server/models/user.model.js
@@ -0,0 +1,19 @@
+module.exports = mongoose => {
+    const schema = mongoose.Schema(
+      {
+        firstName: String,
+        lastName: String,
+        profession: String
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const User = mongoose.model("user", schema);
+    return User;
+  };
