2e72812e245b8821dc58d1377f2898fb1fd1e354
Ay7ot <ayomidotzee@gmail.com>
2023-10-05 06:16:12 -0700

    Fetching user resume complete

--- client/src/Components/App.tsx
+++ client/src/Components/App.tsx
@@ -8,25 +8,30 @@ import { UserDetailsProvider } from "../Contexts/UserDetailContext";
 import { ColorProvider } from "../Contexts/ColorContext";
 import PreviewResume from "./TemplateComponents/PreviewResume";
 import Dashboard from "./DashboardComponents/Dashboard";
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <div className="min-h-[100dvh] bg-[#ffffff] font-Public">
-        <UserDetailsProvider>
-          <ColorProvider>
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/templates" element={<ChooseTemplate />} />
-              <Route path="/edit-template" element={<EditTemplate />} />
-              <Route path="/preview" element={<PreviewResume />} />
-            </Routes>
-          </ColorProvider>
-        </UserDetailsProvider>
-      </div>
-    </LocalizationProvider>
+    <QueryClientProvider client={queryClient}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div className="min-h-[100dvh] bg-[#ffffff] font-Public">
+          <UserDetailsProvider>
+            <ColorProvider>
+              <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/templates" element={<ChooseTemplate />} />
+                <Route path="/edit-template" element={<EditTemplate />} />
+                <Route path="/preview" element={<PreviewResume />} />
+              </Routes>
+            </ColorProvider>
+          </UserDetailsProvider>
+        </div>
+      </LocalizationProvider>
+    </QueryClientProvider>
   )
 }
 
--- client/src/Components/DashboardComponents/DashboardBody.tsx
+++ client/src/Components/DashboardComponents/DashboardBody.tsx
@@ -1,17 +1,32 @@
-import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import { useGeneralAppContext } from "../../Functions/useGeneralAppContext"
+import { useQuery } from 'react-query'
 
 export default function DashboardBody() {
 
-    const navigateTo = useNavigate()
+    const { currentUser } = useGeneralAppContext()
+    const name = currentUser?.displayName || 'User'
 
-    function createNewResume() {
-        navigateTo('/templates')
+    async function fetchResumes() {
+        const firebaseUid = currentUser?.uid;
+        const resumesInfo = await axios.get(`${import.meta.env.VITE_SERVER_URL}userResume/resume`, {
+            params: { firebaseUid } // Send firebaseUid as a query parameter
+        })
+        return resumesInfo.data.message
     }
 
+    const { data, isLoading, error } = useQuery('resumes', fetchResumes)
+
+    console.log(data)
     return (
-        <section className="max-w-[600px] lg:max-w-[800px">
-            <div className="flex items-end">
-                <button onClick={createNewResume} className={`text-[#192657] hidden md:block rounded-md py-3 px-7 border-[1px] border-[#192657] newTemplateGradient font-medium duration-300 ease-in transition-colors`}>Create a new template</button>
+        <section className=" w-full py-6 md:pt-16 px-8 md:px-16">
+            <div className=" border-b-[1px] py-4">
+                <h2 className="text-[#121212] text-[1.5rem] font-medium leading-[3rem] md:text-[2rem]">{`Hello ${name}`}</h2>
+                <p className="text-[#9d9d9d] leading-6">Create your Resume</p>
+            </div>
+            <div className="mt-8">
+                <h3 className="text-[121212] font-medium leading-8">My Resumes</h3>
+
             </div>
         </section>
     )
